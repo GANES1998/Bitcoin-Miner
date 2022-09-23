5b23bf138e7d6499ffffa2c0a533ef028d88248d
GANES1998 <ganes01ravi@gmail.com>
2022-09-23 19:16:34 -0400

    Erlang binaries moved to constants

--- project1.sh
+++ project1.sh
@@ -7,4 +7,4 @@ source constants.env
 export MASTER_NODE=$MASTER_NODE
 export DEDICATED_WORKER_NODE=$DEDICATED_WORKER_NODE
 
-/opt/homebrew/Cellar/erlang/25.0.2/lib/erlang/bin/erl -pa "ebin" -setcookie "${ERLANG_COOKIE}" -name "${MASTER_NODE}" -eval "mine_supervisor:supervise($1)." -s init stop -noshell
+$ERLANG_BIN -pa "ebin" -setcookie "${ERLANG_COOKIE}" -name "${MASTER_NODE}" -eval "mine_supervisor:supervise($1)." -s init stop -noshell
